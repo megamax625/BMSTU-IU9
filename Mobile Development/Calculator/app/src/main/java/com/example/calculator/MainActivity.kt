c1e431b61f00fde29e38abe3f9bf3fa59d289bc9
megamax625 <44984180+megamax625@users.noreply.github.com>
2024-03-26 19:57:14 +0300

    Add files via upload

---
+++ Mobile Development/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,170 @@
+package com.example.calculator
+
+
+//import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.ExpressionBuilder
+
+// exponentation: 2 ^ 2
+// exp: euler's number raised to the power (e^x)
+// sinh: hyperbolic sine
+
+class MainActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        /*Number Buttons*/
+        val tvExpression: TextView = findViewById(R.id.tvExpression)
+
+        val tvOne: TextView = findViewById(R.id.tvOne)
+        tvOne.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "1"
+        }
+
+        val tvTwo: TextView = findViewById(R.id.tvTwo)
+        tvTwo.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "2"
+        }
+
+        val tvThree: TextView = findViewById(R.id.tvThree)
+        tvThree.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "3"
+        }
+
+        val tvFour: TextView = findViewById(R.id.tvFour)
+        tvFour.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "4"
+        }
+
+        val tvFive: TextView = findViewById(R.id.tvFive)
+        tvFive.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "5"
+        }
+
+        val tvSix: TextView = findViewById(R.id.tvSix)
+        tvSix.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "6"
+        }
+
+        val tvSeven: TextView = findViewById(R.id.tvSeven)
+        tvSeven.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "7"
+        }
+
+        val tvEight: TextView = findViewById(R.id.tvEight)
+        tvEight.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "8"
+        }
+
+        val tvNine: TextView = findViewById(R.id.tvNine)
+        tvNine.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "9"
+        }
+
+        val tvZero: TextView = findViewById(R.id.tvZero)
+        tvZero.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "0"
+        }
+
+        /*Operators*/
+
+        // my operators
+
+        val tvExponentiation: TextView = findViewById(R.id.tvExponentiation)
+        tvExponentiation.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "^"
+        }
+
+        val tvExp: TextView = findViewById(R.id.tvExp)
+        tvExp.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "e^"
+        }
+
+        val tvSinh: TextView = findViewById(R.id.tvSinh)
+        tvSinh.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "sinh"
+        }
+
+        val tvOpenBracket: TextView = findViewById(R.id.tvOpenBracket)
+        tvOpenBracket.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "("
+        }
+
+        val tvClosedBracket: TextView = findViewById(R.id.tvClosedBracket)
+        tvClosedBracket.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + ")"
+        }
+
+        // end of my operators
+
+        val tvPlus: TextView = findViewById(R.id.tvPlus)
+        tvPlus.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "+"
+        }
+
+        val tvMinus: TextView = findViewById(R.id.tvMinus)
+        tvMinus.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "-"
+        }
+
+        val tvMul: TextView = findViewById(R.id.tvMul)
+        tvMul.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "*"
+        }
+
+        val tvDivide: TextView = findViewById(R.id.tvDivide)
+        tvDivide.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "/"
+        }
+
+        val tvDot: TextView = findViewById(R.id.tvDot)
+        tvDot.setOnClickListener {
+            tvExpression.text = tvExpression.text.toString() + "."
+        }
+
+        val tvClear: TextView = findViewById(R.id.tvClear)
+
+        val tvResult: TextView = findViewById(R.id.tvResult)
+        tvClear.setOnClickListener {
+            tvExpression.text = ""
+            tvResult.text = ""
+        }
+
+        val tvEquals: TextView = findViewById(R.id.tvEquals)
+        tvEquals.setOnClickListener {
+            val text = tvExpression.text.toString()
+            val expression = ExpressionBuilder(text).build()
+
+            val result = expression.evaluate()
+            val longResult = result.toLong()
+            if (result == longResult.toDouble()) {
+                tvResult.text = longResult.toString()
+            } else {
+                tvResult.text = result.toString()
+            }
+        }
+        val tvBack: TextView = findViewById(R.id.tvBack)
+        tvBack.setOnClickListener {
+            val text = tvExpression.text.toString()
+            if(text.isNotEmpty()) {
+                tvExpression.text = text.drop(1)
+            }
+
+            tvResult.text = ""
+        }
+
+    }
+
+
+
+
+
+
+
+}
